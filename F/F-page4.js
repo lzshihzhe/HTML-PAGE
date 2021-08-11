b6e680ee91add2d3beac5608fae13cc65eac1762
lzshihzhe <jackie5569587@gmail.com>
2021-08-11 16:52:27 +0800

    Update F-page4.js

--- F/F-page4.js
+++ F/F-page4.js
@@ -1,9 +1,73 @@
 
+let a=10
+a+=110
 
+console.log('a',a)
 
-for(let i=2; i<20; i+=3)
+for(let i=20; i<40; i+=5)
     {console.log('i',i)}
   
 
 
+let classA =[1.1, 1.2, 1.3,1.4,1.5]  
+
+
+
+//for(let i=1; i<4;i+=2  )
+//{console.log(classA[i]) }
+
+
+//let classB=[2.1,2.1,2.3,2.4,2.5] 
+//for(a=2;a<5;a+=1)
+//{console.log(classB[a])}
+
+//classA.push(40)
+
+//for( let i= 2 ; i< 6; i=i+2){
+ //   if(i || 2){
+//    classA[i]=999
  
+console.log(classA)
+
+
+const A1=
+    {
+        name :'布魯斯',
+        desc:'貼文一',
+        No:'26'
+    }
+
+    console.log(A1.No)
+
+
+// wdwd//
+// const posts=[
+// {
+//    name :'布魯斯',
+//    desc:'貼文一'
+// },
+// {   name :'布魯斯2號',
+//    desc:'貼文二'
+// },
+// {   name :'布魯斯3號',
+//    desc:'貼文三'
+// },
+
+// ]
+// for (let i = 0 ; i <posts.length;  i++ ){
+//    if ( i===0)  {let post = posts[i]
+//    console.log(posts)
+// }
+// }
+
+let condition=true
+let target =10
+let i= 0
+while (condition)
+{if (i=== target){
+    cpmdotion= true
+}
+console.log (i)
+i++
+}
+
