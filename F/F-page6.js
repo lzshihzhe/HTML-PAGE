c2de644a5570483b9b78911281ad3ba644b54b78
lzshihzhe <jackie5569587@gmail.com>
2021-08-11 16:52:33 +0800

    Create F-page6.js

---
+++ F/F-page6.js
@@ -0,0 +1,118 @@
+
+
+ window.addEventListener('load',function(){ 
+  
+
+/////////////////////////////////////////////////////////////
+
+
+
+
+let isfake= true
+console.log(isfake)
+
+
+let bag=undefined
+console.log(bag);
+
+
+let bag1=null
+console.log(bag1);
+
+const name = "提示"
+let title ="錯誤"
+let title2 ="成功"
+console.log(title)
+
+console.log(100*3);
+
+const a1 ="aaa"
+console.log(a1)
+
+
+
+let classA = ["小贏","小生","小正"]
+classA.push ("小千")
+    
+console.log(classA.length)
+
+const card ={
+    name:`32`,
+    age: `24`,
+    comment:["自屆","內容"]
+}
+
+console.log (card.comment)
+
+
+
+    
+// const video =document.querySelector("#video");
+// window.navigator.mediaDevices.getUserMedia({audio:false,video:true}).then((stream)=>{
+//     video.srcObject=stream;
+//     video.play();
+//  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}) 
+
+
+  
