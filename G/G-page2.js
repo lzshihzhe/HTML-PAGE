eab1d6425fdcc6524c12e1ff3c29bde40f74ba70
lzshihzhe <jackie5569587@gmail.com>
2021-11-13 23:46:06 +0800

    Create G-page2.js

---
+++ G/G-page2.js
@@ -0,0 +1,35 @@
+
+
+
+
+// function Play1(){
+    //     setTimeout(function(){
+    //         console.log(`play1執行中`)
+    //     } ,3000)
+    // }
+    // function Play2(){
+    //     setTimeout(function(){
+    //         console.log(`play2執行中`)
+    //     },5000)
+    // }
+    // console.log("程式開始")
+    // Play1()
+    // Play2()
+    // console.log("程式結束")
+
+
+    var myName = "Kumar";
+    var myStr = "My name is " + myName + "and I am swell!";
+    console.log(myStr)
+    
+//////////////////////////////////////////////////////////////
+
+let rickyAndCal = "Cal and Ricky both like racing.";
+let calRegex = /$/; // Change this line
+let result = calRegex.test(rickyAndCal);
+console.log(result)
+
+
+link rel="stylesheet" href=".." 
+integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
+crossorigin="anonymous"/>
